2db5652d7b46423201826f2a809b994e0f3b2df3
Luis David Cornejo <luisdavidcorbra24@gmail.com>
2022-11-21 16:25:54 -0500

    Post
    En general, uso de método post para enviar y recibir datos del servidor

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 app.use(express.static('public'))
+app.use(cors())
+app.use(express.json())
+
+app.post("/enviar", (req, res) => {
+    const mensaje = req.body.mensaje || ""
+    console.log(mensaje);
+    res.send({mensaje})
+})
 
 app.listen(8080, () => {
     console.log("Server running...");
